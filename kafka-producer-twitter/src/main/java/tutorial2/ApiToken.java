de3c5bd8233c98fad90c22854e99a10791189d6a
Shao-Eric <erichu11364@gmail.com>
2019-07-07 18:23:57 -0400

    Delete ApiToken.java

--- kafka-producer-twitter/src/main/java/tutorial2/ApiToken.java
+++
@@ -1,8 +0,0 @@
-package tutorial2;
-
-public class ApiToken {
-    public static final String consumerKey = "IuQgUkmvvb4H1EXixVvry7ZgU";
-    public static final String consumerSecret = "4yWWwEnzQ3TzVL4qNvGefVBv388Jy9trjjcobc018l4RyYI2nT";
-    public static final String token = "1147587560216154117-fKAYWD2z93hdWf5p0l3bWIiXqCmyuQ";
-    public static final String secret = "Zdiu6uAVs3uyIlQ5JJQVH7Kg5JCQE8xF2ZPUokdFu85T7";
-}
